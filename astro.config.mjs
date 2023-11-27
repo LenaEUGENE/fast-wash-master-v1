4ed51e38aeb4461138870e739315290caad092ad
Lena EUGENE <lenaeugene@gmail.com>
2023-11-27 19:08:14 +0100

    add astro-icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
+
+import icons from "astro-icons";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +12,5 @@ export default defineConfig({
     extendMarkdownConfig: false,
     smartypants: true,
     gfm: true
-  }), sitemap(), tailwind()]
+  }), sitemap(), tailwind(), icons()]
 });
