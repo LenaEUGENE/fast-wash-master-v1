65f2279df265c1fb230655b01372f600dbd4ba06
Lena EUGENE <lenaeugene@gmail.com>
2023-11-28 13:43:10 +0100

    stuffs

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Fast Wash Master';
+export const SITE_DESCRIPTION = 'Nous nettoyons votre véhicule dans le confort de votre domicile!';
